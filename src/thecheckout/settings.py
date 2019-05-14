3c843ac1034d3c3f3f17b7bb671290a82643b038
mvgoffin <50590174+mvgoffin@users.noreply.github.com>
2019-05-14 15:28:42 +0100

    un # DATABASE sqlite3

--- src/thecheckout/settings.py
+++ src/thecheckout/settings.py
@@ -80,14 +80,14 @@ WSGI_APPLICATION = 'thecheckout.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-#if DEBUG:
-#        DATABASES = {
-#            'default': {
-#                'ENGINE': 'django.db.backends.sqlite3',
-#                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#            }
-#        }
-#else:
+if DEBUG:
+        DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            }
+        }
+else:
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
