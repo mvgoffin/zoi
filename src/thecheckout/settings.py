51bb17528f27bb8fb03e6b74e692b2042b5c3b67
Marco Valle Goffin <marco.valgof@gmail.com>
2019-05-15 18:29:09 +0100

    going Live Stripe Keys

--- src/thecheckout/settings.py
+++ src/thecheckout/settings.py
@@ -154,8 +154,8 @@ if DEBUG:
 # Stripe Keys
 
 #test keys Uncomment to go LIVE!
-STRIPE_PUBLIC_KEY = 'pk_test_BDcILuPUX13SXoMVLNxOojyt'
-STRIPE_PRIVATE_KEY = 'sk_test_p2vTfVUyXWWQtDTAxpP5M3Av'
+#STRIPE_PUBLIC_KEY = 'pk_test_BDcILuPUX13SXoMVLNxOojyt'
+#STRIPE_PRIVATE_KEY = 'sk_test_p2vTfVUyXWWQtDTAxpP5M3Av'
 
 #live keys
 STRIPE_PUBLIC_KEY = 'pk_live_dqYcQQrsiKP9wOLLJTWnvpPc'
