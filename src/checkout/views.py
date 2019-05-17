40123d6e7ddbbdf38b58be33ea982f68efeca3b8
Marco Valle Goffin <marco.valgof@gmail.com>
2019-05-17 11:21:41 +0100

    no card error

--- src/checkout/views.py
+++ src/checkout/views.py
@@ -33,11 +33,11 @@ def checkout_mx(request):
                 source=customer                 #added source customer
             )
 
-        response = redirect('success')
-        return response
+            response = redirect('success')
+            return response
 
-        except stripe.error.CardError as e:
-            pass
+        #except stripe.error.CardError as e:
+        #    pass
 
     context = {'publishKey': publishKey}
     template = 'checkout_mx.html'
