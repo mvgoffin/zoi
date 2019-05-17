a43b66be419f5fcb353edbe0f6e56b24db0ddd7a
Marco Valle Goffin <marco.valgof@gmail.com>
2019-05-17 10:31:34 +0100

    added response after customer creation

--- src/checkout/views.py
+++ src/checkout/views.py
@@ -24,6 +24,7 @@ def checkout_mx(request):
                 description="This is a Test",
                 source=token
             )
+            response = redirect('success')
             except stripe.error.CardError as e:
             pass
 
