efee676a22d6503e1f9c398ba01f38a3009de657
Marco Valle Goffin <marco.valgof@gmail.com>
2019-05-21 18:29:44 +0100

    Views.py Home for access code

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    context = locals()
-    template = 'home.html'
-    return render(request,template,context)
+#def home(request):
+#    context = locals()
+#    template = 'home.html'
+#    return render(request,template,context)
 
 def about(request):
     context = locals()
@@ -20,4 +20,33 @@ def code_granted(request):
     context = locals()
     template = 'code_granted.html'
     return render(request,template,context)
+
+def home(request):
+    form = AccessForm(request.POST)
+
+    email = request.POST.get('email')
+    obj = Account.objects.create(email=email)
+
+    if form.is_valid():
+
+        send_mail(
+        'A new LEAD',
+        'From our servers, a new LEAD has been created.' +
+        ': email: {}'.format(email),
+        'hi@plntprotein.com',
+        ['marco@plntprotein.com'],
+        #fail_silently=False,
+        )
+
+        form.save()
+        
+        response = redirect('code_granted')
+        return response
+    else:
+        form = AccessForm()
+    
+     #context = {'form': form}
+     #template = 'register_mx.html'
+     #return render(request,template,context)
+    return render(request, 'home.html', {'form': form}) #this is render on HTML
 
