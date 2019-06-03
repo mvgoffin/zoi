f900672c9a601374d49dd38e99e9e53f99630f18
Marco Valle Goffin <marco.valgof@gmail.com>
2019-06-03 11:37:04 +0100

    Diabled from

--- src/profiles/forms.py
+++ src/profiles/forms.py
@@ -1,5 +1,5 @@
 from django import forms 
-from profiles.models import Access
+#from profiles.models import Access (diabling PLNT PROTEIN IG landing page)
 from django.forms import TextInput, NumberInput, EmailInput
 
 #Disabling Home PLNT PROTEIN Landing Page
