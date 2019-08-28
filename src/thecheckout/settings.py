60f1cee75184cade8bc0d9cfb4c1e0d45996b32e
Marco Valle Goffin <marco.valgof@gmail.com>
2019-08-28 13:12:49 +0100

    uupdated

--- src/thecheckout/settings.py
+++ src/thecheckout/settings.py
@@ -55,24 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'djangosecure.middleware.SecurityMiddleware',
+    #'djangosecure.middleware.SecurityMiddleware',
 ]
-
-SECURE_SSL_REDIRECT = True
-
-SECURE_HSTS_SECONDS = 1
-
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-
-SECURE_FRAME_DENY = True
-
-SECURE_CONTENT_TYPE_NOSNIFF = True
-
-SECURE_BROWSER_XSS_FILTER = True
-
-SESSION_COOKIE_SECURE = True
-
-SESSION_COOKIE_HTTPONLY = True
 
 ROOT_URLCONF = 'thecheckout.urls'
 
