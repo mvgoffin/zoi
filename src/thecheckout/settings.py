4ed078f28d1764d4a3726a8320b14b50348fe04a
mvgoffin <marco.valgfo@gmail.com>
2019-11-20 17:43:08 +0000

    #staticfiles_DIRS

--- src/thecheckout/settings.py
+++ src/thecheckout/settings.py
@@ -146,10 +146,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = (
-    normpath(join(BASE_DIR, 'static')),
-    normpath(join(BASE_DIR, 'upload')),
-)
+#STATICFILES_DIRS = [os.path.join((BASE_DIR), 'static_in_env')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
