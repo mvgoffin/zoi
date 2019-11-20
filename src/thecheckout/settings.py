cb3ec6b4b8296e45e48c8bead609632bf1b6415f
mvgoffin <marco.valgfo@gmail.com>
2019-11-20 17:38:07 +0000

    change from static_in_env to static_root

--- src/thecheckout/settings.py
+++ src/thecheckout/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join((BASE_DIR), 'static_in_env')]
+STATICFILES_DIRS = [os.path.join((BASE_DIR), 'static_root')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
