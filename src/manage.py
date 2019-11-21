1bd76d78123a53b023c1aaf51fbb6598cdb7b2b8
mvgoffin <marco.valgfo@gmail.com>
2019-11-21 11:14:46 +0000

    updated manage.py

--- src/manage.py
+++ src/manage.py
@@ -3,10 +3,10 @@
 import os
 import sys
 
-
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thecheckout.settings')
-    try:
+
+ttry:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
