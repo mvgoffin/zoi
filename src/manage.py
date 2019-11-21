d31c87c5ebe8fd4a7f45e4a675d55ebe09f1caaf
Marco VG <test@iphone.home>
2019-11-21 18:13:20 +0000

    update manage.py

--- src/manage.py
+++ src/manage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -13,7 +14,7 @@ try:
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        )from exc
     execute_from_command_line(sys.argv)
 
 
