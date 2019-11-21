d39a9aee057a9710e5b7d8813ef1c9eea1621d2d
mvgoffin <marco.valgfo@gmail.com>
2019-11-21 10:26:35 +0000

    updated settings

--- src/manage.py
+++ src/manage.py
@@ -3,9 +3,8 @@
 import os
 import sys
 
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thecheckout.settings')
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thecheckout.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
