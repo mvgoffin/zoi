20f4a3e3f808c98777c5b67eb5cbf960fb623957
Marco VG <test@Marcos-MacBook-Pro.local>
2019-11-23 11:58:10 +0000

    deleted smsdelete success-account

--- thecheckout/urls.py
+++ thecheckout/urls.py
@@ -56,7 +56,7 @@ urlpatterns = [
     path('privacy-policy/', views.privacypolicy, name='privacy-policy'),
     path('sms-policy/', views.smspolicy, name='sms-policy'),
     path('success/', views.success, name='success'),
-    path('success-account/', views.successaccount, name='success-account'),
+    #path('success-account/', views.successaccount, name='success-account'),
     #path('hometest/', views.hometest, name='hometest'),
     #path('code_granted/', views.code_granted, name='code_granted'),
     
