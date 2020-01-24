40283cf725aa1da7c76f890f6c0c15a90efcb83b
mvgoffin <marco.valgof@gmail.com>
2020-01-24 11:15:56 +0000

    updated

--- plntprotein/urls.py
+++ plntprotein/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('', views.home),
     path('admin/', admin.site.urls),
     path('home/', views.home, name='home'),
+     path('test/', views.test, name='test'),
     
     path('thankyou/', views.thankyou, name='thankyou'),
     
