26592242046ea8d1734a512fdd44dae2125937a4
mvgoffin <marco.valgof@gmail.com>
2020-04-19 11:53:39 +0100

    added Postgres DB

--- dak/settings.py
+++ dak/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'lzonf2g-*0*244y@zh45x^jkszou13^+2bm*y)$o23kb&ur-_'
 DEBUG = False
 # True - Environment // False - Production
 
-ALLOWED_HOSTS = ['localhost', '198.199.123.95', 'www.dakdemo.com', 'dakdemo.com']
+ALLOWED_HOSTS = ['localhost', '198.199.123.95', 'www.dakdemo.com', 'dakdemo.com', 'www.demodap.com', 'demodap.com']
 
 # Application definition
 
@@ -89,10 +89,10 @@ if DEBUG:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'alteredaidb',
-            'USER': 'dak',
-            'PASSWORD': 'mysql4444',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'dapproject',
+            'USER': 'dapuser',
+            'PASSWORD': 'dappw1972',
             'HOST': 'localhost',
             'PORT': '',
         }
