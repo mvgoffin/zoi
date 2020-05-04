452157a192cc84f71a36db3efde1c10815f8c91f
mvgoffin <marco.valgof@gmail.com>
2020-05-04 08:32:03 +0100

    updated

--- dak/urls.py
+++ dak/urls.py
@@ -35,7 +35,7 @@ urlpatterns = [
     path('pagelseg/', views.pagelseg, name='pagelseg'),
     path('pagelseg_john/', views.pagelseg_john, name='pagelseg_john'),
 
-    #path('loginvf_john/', views.loginvf_john, name='loginvf_john'),
+    path('loginvf_john/', views.loginvf_john, name='loginvf_john'),
     #path('pagevf_john/', views.pagevf_john, name='pagevf_john'), 
     #path('pagevf_ariana/', views.pagevf_ariana, name='pagevf_ariana'), 
 ]
