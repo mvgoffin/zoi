8508d3abb5848d31dbef4b6c91f7cc22b385b168
mvgoffin <marco.valgof@gmail.com>
2020-07-25 16:45:21 +0100

    updated

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404, HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
+from .form import *
 
 #Contact form view
 
