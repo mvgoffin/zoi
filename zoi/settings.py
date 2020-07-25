3cbcf858d3b5f70478e47ce6f940a43b4e0a3db1
mvgoffin <marco.valgof@gmail.com>
2020-07-25 09:20:58 +0100

    updated

--- zoi/settings.py
+++ zoi/settings.py
@@ -152,6 +152,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = "marco.valgof@gmail.com"
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_PASSWORD = 'Google1607'
 EMAIL_PORT = '587'
 
