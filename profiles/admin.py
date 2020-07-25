8d647a909a6b4711a9c0da408b565d28d92cb6b7
mvgoffin <marco.valgof@gmail.com>
2020-07-25 09:35:19 +0100

    updated

--- profiles/admin.py
+++ profiles/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from users.models import Account
+#from users.models import Account
 
 #Disabling Home 
-class accountadmin(admin.ModelAdmin):
-    class Meta:
-        model = Account
+#class accountadmin(admin.ModelAdmin):
+#    class Meta:
+#        model = Account
 
-admin.site.register(Account, accountadmin)
+#admin.site.register(Account, accountadmin)
