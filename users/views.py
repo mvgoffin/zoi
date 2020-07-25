134370c76659ad780d2cdb73e57f92a5f3484b69
mvgoffin <marco.valgof@gmail.com>
2020-07-25 10:06:25 +0100

    updated

--- users/views.py
+++ users/views.py
@@ -34,7 +34,7 @@ def get_name(request):
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
+            return HttpResponseRedirect('thanks.html')
 
     # if a GET (or any other method) we'll create a blank form
     else:
