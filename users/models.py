de8e83f0050900d7eb7a9397e892e6b59486b230
mvgoffin <marco.valgof@gmail.com>
2020-07-25 16:08:11 +0100

    updated

--- users/models.py
+++ users/models.py
@@ -1,16 +1,3 @@
-#from django.db import models
 
-#from phonenumber_field.modelfields import PhoneNumberField
-
-# Create your models here.
-#class Account(models.Model):
-#    name = models.CharField(max_length = 100, null=True)
-#    email = models.EmailField(max_length = 100, null=True) #by default is required
-
-#class Access(models.Model):
-#    email = models.EmailField(max_length = 100, null=True) #by default is required
-
-#def _unicode_(self):
-#        return self.id
 
 
