6a7465c8f7c9ca72c0a68801f6952825343073b2
mvgoffin <marco.valgof@gmail.com>
2020-07-25 15:23:35 +0100

    updated

--- users/views.py
+++ users/views.py
@@ -21,26 +21,15 @@
 
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 
 from users.forms import NameForm
 
-def get_name(request):
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('thanks.html')
-
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = NameForm()
-
-    return render(request, 'thanks.html', {'form': form})
-
-    if request.method == 'POST' and your_name and your_email:
-        send_mail(subject, content, settings.EMAIL_HOST_USER, ['marco.valgof@gmail.com'], fail_silently=False)
+def email(request):
+    subject = 'Thank you for registering to our site'
+    message = ' it  means a world to us '
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = ['marco.valgof@gmail.com',]
+    send_mail( your_name, your-email )
+    return redirect('redirect to a new page')
