f28759c5d846ff37bfc0f67678b5f51f7a0662ec
mvgoffin <marco.valgof@gmail.com>
2020-07-25 16:32:49 +0100

    updated

--- users/views.py
+++ users/views.py
@@ -7,23 +7,23 @@ def Contact(request):
         form = Contact_Form(data=request.POST)
 
         if form.is_valid():
+            contact_name = request.POST.get('contact_name')
+            contact_email = request.POST.get('contact_email')
+            contact_content = request.POST.get('content')
 
             template = get_template('profiles/templates/contact_form.text')
             content = (
-                contact_name = request.POST.get('contact_name')
-                contact_email = request.POST.get('contact_email')
-                contact_content = request.POST.get('content')
+                'contact_name' : request.POST.get('contact_name'),
+                'contact_email' : request.POST.get('contact_email'),
+                'contact_content' : request.POST.get('contact_content'),
             )
 
             content = template.render(content)
 
-
-
-           
              email = EmailMessage(
                 "New contact form email",
                 content,
-                "creative Web" + '',
+                "Creative Web" + '',
                 ['marco.valgof@gmail.com'],
                 header = {'Reply To': contact_email }
         )
