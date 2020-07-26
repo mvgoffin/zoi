c06aca208dee2b56739dd622a229b9006197fd9f
mvgoffin <marco.valgof@gmail.com>
2020-07-26 08:04:19 +0100

    updated

--- profiles/views.py
+++ profiles/views.py
@@ -26,3 +26,5 @@ def contactpage(request):
 
 
 
+
+
---
+++ users/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from forms import contactformemail
+from django.core.mail import send_mail
+
+def contactsendmail(request):
+    if request.method=="GET":
+        form=contactformemail()
+        return render(request,'contactpage.html',{'form':form})
