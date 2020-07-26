691948216604f7799ebfced14ab1955c98b1693d
mvgoffin <marco.valgof@gmail.com>
2020-07-26 07:58:41 +0100

    updated

--- users/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from forms import contactformemail
-from django.core.mail import send_mail
-
-def contactsendmail(request):
-    if request.method=="GET":
-        form=contactformemail()
-        return render(request,'contactpage.html',{'form':form})
