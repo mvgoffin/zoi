6880c4e332339c01f9a3d6c6ef6d37bc226f368f
mvgoffin <marco.valgof@gmail.com>
2020-07-26 07:49:47 +0100

    updated

--- zoi/forms.py
+++ zoi/forms.py
@@ -2,6 +2,5 @@ from django import forms
 
 class contactformemail(forms.Form):
     fromemail=forms.EmailField(required=True)
-    subject=form.CharField(required=True)
-    message=form.CharField(required=True)
-    
+    subject=forms.CharField(required=True)
+    message=forms.CharField(required=True)
